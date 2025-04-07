54d8a8151e3f30d38179134a9b848ffdd1c3c89c
DipokRay <dipokray1100@gmail.com>
2025-04-07 20:44:42 +0600

    Protype Model

--- Object Create Mathod /app.js
+++ Object Create Mathod /app.js
@@ -1,6 +1,6 @@
 // Object.create() Method
 
-let person = function () {
+let personOne = function () {
   return `The person is ${this.name} and age is ${this.age} and profession is ${this.pl}`;
 };
 
@@ -12,15 +12,13 @@ let person = function () {
 
 // console.log(person());
 
-let person1 = Object.create(person, {
+let person1 = Object.create(personOne, {
   name: { value: "John" },
   age: { value: 30 },
   pl: { value: "Developer" },
 });
 
-console.log(person());
-
-
+console.log(personOne());
 
 function validateEmail(email) {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -30,3 +28,17 @@ function validateEmail(email) {
 // Example usage:
 console.log(validateEmail("test@example.com")); // true
 console.log(validateEmail("invalid-email")); // false
+
+let person = {
+  info: function () {
+    console.log(
+      `The person name is ${this.name} and he is ${this.age} years old and he is a ${this.pl}`
+    );
+  },
+};
+let randomPerson = Object.create(person);
+randomPerson.name = "John";
+randomPerson.age = 30;
+randomPerson.pl = "Developer";
+
+console.log(rendomPerson.info());
---
+++ Prototype Model/README.md
@@ -0,0 +1,16 @@
+# Prototype Model
+
+## Code
+
+```JS
+let obj = {};
+console.log(obj);
+```
+
+### In this code every `object` of JavaScript has an anonymous property called protype
+
+### - We can check the prototype of something 3 way
+
+1. `obj.__proto__`
+2. `obj.constructor.prototype`
+3. `obj.getPrototypeOf(a)`
