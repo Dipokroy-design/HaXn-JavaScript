e8b8e00ae4ec1846aa0e0ac4d21330b3b72d015f
DipokRay <dipokray1100@gmail.com>
2025-04-07 22:16:38 +0600

    Prototype

--- Prototype Model/app.js
+++ Prototype Model/app.js
@@ -1,5 +1,13 @@
-let obj = {};
+// let obj = {};
 // console.log(obj);
 
-let type = Object.getPrototypeOf(obj);
-console.log(type);
+// let type = Object.getPrototypeOf(obj);
+// console.log(type);
+
+Array.prototype.Hello = function () {
+  console.log(this);
+  return this.length;
+};
+
+let arr = [1, 2, 3];
+console.log(arr.Hello());
